6d7cf72c534bc516b5122f6ed3888e5322bc41dd
Le-Leal <h.okumura0131@gmail.com>
2025-07-28 01:07:44 +0900

    initialized

---
+++ beg/c.cpp
@@ -0,0 +1,89 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define srep(i,l,r) for(ll i=l;i<=r;i++)
+#define irep(i,r,l) for(ll i=r;i>=l;i--)
+using ll = long long;
+using ld = long double;
+const ll mod=998244353;
+#define vout(v) for(auto i :v) cout<<i<<" ";
+#define INF 9223300000000000000ll
+#define Winf 5e12
+#define nl "\n"
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define vl vector<ll>
+#define pb push_back
+#define vc vector<char>
+#define vb vector<bool>
+#define uniq(x) sort((x).begin(),(x).end());(x).erase(unique((x).begin(),(x).end()),(x).end())
+#define eb emplace_back
+
+
+
+void no(void) { cout<<"No"<<nl;}
+void yes(void) {cout<<"Yes"<<nl;}
+void yn(bool a) {
+    cout<<(a ? "Yes":"No")<<nl;
+}
+
+
+vector<ll> dx={-1,0,1,0,1,1,-1,-1};
+vector<ll> dy={0,-1,0,1,-1,1,-1,1};
+
+bool isin(ll i,ll j,ll h,ll w) {
+    if(i<0 || i>=h || j<0 || j>=w) return false;
+    return true;
+}
+
+template<typename T> bool chmin(T& a, T b){if(a > b){a = b; return true;} return false;}
+template<typename T> bool chmax(T& a, T b){if(a < b){a = b; return true;} return false;}
+
+
+template<class T>T vecmax(const vector<T>&v){return *max_element(all(v));}
+template<class T>T vecmin(const vector<T>&v){return *min_element(all(v));}
+
+ll safemod(ll num,ll rule) {
+    return (num%rule+rule)%rule;
+}
+//ファイル読み込みは第二フォルダから ex:include "mathtype/hoge.hpp"
+
+ll modpow(ll fl, ll po, ll mode) {  // mode: 0=modなし, 1=modあり
+    ll ret=1;
+    if (mode) {
+        while (po>0) {
+            if (po&1) ret=(ret*fl)%mod;
+            fl=(fl*fl)%mod;
+            po>>=1;
+        }
+    } else {
+        while (po>0) {
+            if(po&1) ret*=fl;
+            fl*=fl;
+            po>>=1;
+        }
+    }
+    return ret;
+}
+
+int main() {
+    ll n,k,x; cin>>n>>k>>x;
+    x--;
+    vector<string> ss(n);
+    rep(i,n) cin>>ss[i];
+    vector<string> ms;
+    ll mp=modpow(n,k,0);
+    rep(i,mp) {
+        string str;
+        ll ic=i;
+        rep(j,k) {
+            str+=ss[ic%n];
+            ic/=n;
+        }
+        ms.pb(str);
+    }
+    sort(all(ms));
+    cout<<ms[x]<<nl;
+}
